018cd4b2c13377fa061584f1cb0f504f4a1bb5c9
Qin Ying Chen <qinyingchen@qycs-MBP.fios-router.home>
2021-09-24 02:28:33 -0400

    adding code and readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# flix
+# Flix
+
+Flix is an app that allows users to browse movies from the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+
+## Flix Part 1
+
+### User Stories
+
+#### REQUIRED (10pts)
+- [x] (2pts) User sees an app icon on the home screen and a styled launch screen.
+- [x] (5pts) User can view and scroll through a list of movies now playing in theaters.
+- [x] (3pts) User can view the movie poster image for each movie.
+
+#### BONUS
+- [x] (2pt) User can view the app on various device sizes and orientations.
+- [ ] (1pt) Run your app on a real device.
+
+### App Walkthrough GIF
+<img src= width=250><br>
+
+GIF created with QuickTime Player and [ezgif.com](https://ezgif.com/video-to-gif)
+
+### Notes
+Challenges faced:
+1. understanding and working with the syntax
---
+++ flixster/flixster/MoviesViewController.swift
@@ -0,0 +1,81 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Qin Ying Chen on 9/23/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    // properties (available for the lifetime of this screen...)
+    var movies = [[String:Any]]()   // declare array of dictionaries
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // network request snippet
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                    // get the array of movies & store movies to use elsewhere
+                    self.movies = dataDictionary["results"] as! [[String:Any]]   // as! = casting as the type that follows
+                    
+                    // TODO: Reload your table view data
+                    self.tableView.reloadData()     // needs to call this manually or the screen will not update!
+             }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count     // returns the size of the movies array
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // allows for recycling of cell & cast it as MovieCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie = movies[indexPath.row]   // get the movie at the row index
+        let title = movie["title"] as! String   // get the title and cast it to String
+        let synopsis = movie["overview"] as! String
+
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        
+        // get poster URL from api
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL+posterPath)
+        
+        // use AlamofireImage to grab poster from URL and display it
+        cell.posterView.af.setImage(withURL: posterURL!)
+        
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
